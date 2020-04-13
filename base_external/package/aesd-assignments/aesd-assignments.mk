827e43d172e86c8293014199da979ee82fe80274
ruchaborwankar <Rucha.Borwankar@colorado.edu>
2020-04-12 22:18:56 -0600

    enabling uart on buildroot

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,9 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = b6306651819f8a276f67942ed0364544f0270dde
+#old version working:AESD_ASSIGNMENTS_VERSION = a6d535254f6568a8cb515b56399ab9e754bda70a
+
+AESD_ASSIGNMENTS_VERSION = a6d535254f6568a8cb515b56399ab9e754bda70a 
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-atharvdesai1996.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -15,10 +17,13 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
+
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/test $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/echopc $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/writetotiva $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/writetobb $(TARGET_DIR)/bin
 endef
-
 
 $(eval $(generic-package))
